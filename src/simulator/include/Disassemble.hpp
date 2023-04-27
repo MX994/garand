6d77f24cf87b1dc3ac0b541da6ec42123fcee305
Dung Tuan Nguyen <ntddebugger@gmail.com>
2023-04-27 08:10:09 -0400

    add: basic diasm

---
+++ src/simulator/include/Disassemble.hpp
@@ -0,0 +1,12 @@
+#include "Memory.hpp"
+#include "Instruction.hpp"
+#include <string>
+
+#ifndef GARAND_DISASSEMBLE_HPP
+#define GARAND_DISASSEMBLE_HPP
+
+namespace Garand {
+std::string disassemble(GarandInstruction ins);
+} // namespace Garand
+
+#endif
